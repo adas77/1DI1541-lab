fcdae0085f73dd9be842ddcec08fb72d4427145d
Adam Pilewski <apilewski01@gmail.com>
2022-11-09 02:02:39 +0100

    run ps1

--- run_docker.sh
+++ run_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export $(grep -v '^#' .env | xargs -d '\n')
-[ -n "$(docker images -q $DOCKER_IMAGE_NAME)" ] || sudo docker build --tag $DOCKER_IMAGE_NAME .
-sudo docker build --tag $DOCKER_IMAGE_NAME .
+[ -n "$(docker images -q $DOCKER_IMAGE_NAME)" ] || docker build --tag $DOCKER_IMAGE_NAME .
+docker build --tag $DOCKER_IMAGE_NAME .
 echo "RUNNING ON PORT: $FLASK_DOCKER_PORT"
-sudo docker run -v $DOCKER_DB_PATH:/src/instance --publish $FLASK_DOCKER_PORT:5000 $DOCKER_IMAGE_NAME
+docker run -v $DOCKER_DB_PATH:/src/instance --publish $FLASK_DOCKER_PORT:5000 $DOCKER_IMAGE_NAME
