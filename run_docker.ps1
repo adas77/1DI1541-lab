4499270366bd84cf81b538edf243ed632c19dfeb
Adam Pilewski <adam.pilewski.stud@pw.edu.pl>
2022-11-09 03:23:12 +0100

    windows

--- run_docker.ps1
+++ run_docker.ps1
@@ -1,16 +1,16 @@
 get-content .env | ForEach-Object {
     if ($_) {
         $name, $value = $_.split('=')
+        set-content env:\$name $value
     }
-    set-content env:\$name $value
+    
 }
 
-# use sudo: https://support.circleci.com/hc/en-us/articles/360045816254-How-to-sudo-on-Powershell-Windows-executor
-$img = sudo docker images -q ${env:DOCKER_IMAGE_NAME}
+$img = docker images -q ${env:DOCKER_IMAGE_NAME}
 
 if (-Not $img ) {
-    sudo docker build --tag ${env:DOCKER_IMAGE_NAME} .
+    docker build --tag ${env:DOCKER_IMAGE_NAME} .
 }
-sudo docker build --tag ${env:DOCKER_IMAGE_NAME} .
+docker build --tag ${env:DOCKER_IMAGE_NAME} .
 Write-Output "RUNNING ON PORT: ${env:FLASK_DOCKER_PORT}"
-sudo docker run -v ${env:DOCKER_DB_PATH}:/src/instance --publish ${env:FLASK_DOCKER_PORT}:5000 ${env:DOCKER_IMAGE_NAME}
+docker run -v ${env:WINDOWS_DOCKER_DB_PATH}:/src/instance --publish ${env:FLASK_DOCKER_PORT}:5000 ${env:DOCKER_IMAGE_NAME}
