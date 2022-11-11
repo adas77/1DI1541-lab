260410f561cb3359b7c089b527db4768a25e4096
Adam Pilewski <apilewski01@gmail.com>
2022-11-11 20:08:51 +0100

    .

--- run_docker.ps1
+++ run_docker.ps1
@@ -6,10 +6,11 @@ get-content .env | ForEach-Object {
     
 }
 
-$img = docker images -q ${env:DOCKER_IMAGE_NAME}
+# $img = docker images -q ${env:DOCKER_IMAGE_NAME}
 
-if (-Not $img ) {
-    docker build --tag ${env:DOCKER_IMAGE_NAME} .
-}
+# if (-Not $img ) {
+#     docker build --tag ${env:DOCKER_IMAGE_NAME} .
+# }
+docker build --tag ${env:DOCKER_IMAGE_NAME} .
 Write-Output "RUNNING ON PORT: ${env:FLASK_DOCKER_PORT}"
 docker run -v ${env:WINDOWS_DOCKER_DB_PATH}:/src/instance --publish ${env:FLASK_DOCKER_PORT}:5000 ${env:DOCKER_IMAGE_NAME}
